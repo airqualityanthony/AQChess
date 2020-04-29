3fb58aa7990f87fca2f8a3af4857807705eabdbf
airqualityanthony <walker.anthony91@gmail.com>
2020-04-29 20:24:25 +0100

    added latest game to the home page

--- flaskapp.py
+++ flaskapp.py
@@ -98,7 +98,7 @@ names = ["Anthony","Josh","Pauls","Naval","Lee","Alex","James","Girish"]
 user_list = ["ElectricFalcon", "JPeg71","paulsilzins","ChessWho1","ldavie2","mralexbarr","Relph29","HardAmmo"]
 ## run win_list function to retrieve number of wins for each player.
 wins = win_list(user_list, month="04",year="2020")
-pgn_list = games_return(user_list[0], month= "03",year="2020")
+pgn_list = games_return(user_list[0], month= "04",year="2020")
 details = list(zip(names,user_list,wins))
 ordered_details = sorted(details,key=lambda x:x[2], reverse = True)
 # get PGN string for chess.js to read.
@@ -132,13 +132,45 @@ Naval_games = games_list(games_return(user_list[3], month= "04",year="2020"))
 Lee_games = games_list(games_return(user_list[4], month= "04",year="2020"))
 Alex_games = games_list(games_return(user_list[5], month= "04",year="2020"))
 James_games = games_list(games_return(user_list[6], month= "04",year="2020"))
-Girsh_games = games_list(games_return(user_list[7], month= "04",year="2020"))
+Girish_games = games_list(games_return(user_list[7], month= "04",year="2020"))
 
-g = Anthony_games+Josh_games+Pauls_games+Naval_games+Lee_games+Alex_games+James_games+Girsh_games
+g = Anthony_games+Josh_games+Pauls_games+Naval_games+Lee_games+Alex_games+James_games+Girish_games
 ## remove duplicates
 import itertools
 g.sort()
 new_num = list(num for num,_ in itertools.groupby(g))
+
+Anthony_moves = games_return(user_list[0], month= "04",year="2020")
+Josh_moves = games_return(user_list[1], month= "04",year="2020")
+Pauls_moves = games_return(user_list[2], month= "04",year="2020")
+Naval_moves = games_return(user_list[3], month= "04",year="2020")
+Lee_moves = games_return(user_list[4], month= "04",year="2020")
+Alex_moves = games_return(user_list[5], month= "04",year="2020")
+James_moves = games_return(user_list[6], month= "04",year="2020")
+Girish_moves = games_return(user_list[7], month= "04",year="2020")
+
+g_moves = Anthony_moves+Josh_moves+Pauls_moves+Naval_moves+Lee_moves+Alex_moves+James_moves+Girish_moves
+## remove duplicates
+import itertools
+g_moves.sort()
+moves_clean = list(num for num,_ in itertools.groupby(g_moves))
+
+games_total = []
+for i in moves_clean:
+    games = []
+    pgn = io.StringIO(i)
+    game = chess.pgn.read_game(pgn)
+    move = str(game.mainline())
+    games.append(move)
+    white_p = game.headers['White']
+    black_p = game.headers['Black']
+    date = game.headers['EndDate']
+    time = game.headers['EndTime']
+    dt = str(date + " " + time)
+    games.append(white_p)
+    games.append(black_p)
+    games.append(dt)
+    games_total.append(games)
 
 
 games_march = games_list(pgn_list)
@@ -149,19 +181,19 @@ details_april = list(zip(names,user_list,wins))
 ordered_details_april = sorted(details_april,key=lambda x:x[2], reverse = True)
 games_april = games_list(pgn_list_april)
 
-
-
+latest_game = games_total[-1]
+latest_game_moves = games_total[-1][0]
 
 @app.route("/")
 def home():
-	return render_template("index.html", details = ordered_details, games = new_num)
+	return render_template("index.html", details = ordered_details, games = new_num, latest_game = latest_game, latest_moves = latest_game_moves)
 @app.route("/april")
 def april():
     return render_template("april.html", details = ordered_details_april, games = new_num)
 
-@app.route('/<username>')
-def username_games(username):
-    return render_template("usernamegames.html", details = username)
+@app.route('/band')
+def band():
+    return render_template("band.html")
 
 @app.route("/games")
 def games():
